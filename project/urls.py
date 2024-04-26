54527dca771d11ae4f4089f6dc2b1da6979997a6
Jeet Desai <jeetdesai995@gmail.com>
2024-04-26 13:26:27 +0530

    update url

--- project/urls.py
+++ project/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/', views.Projects.as_view(), name="get projects"),
+    path('projects', views.Projects.as_view(), name="get projects"),
 ]
--- project/views.py
+++ project/views.py
@@ -9,11 +9,10 @@ class Projects(APIView):
     permission_classes = [IsAuthenticated]
     def get(self, request, format=None):
         user = request.user
-        try:
-            memberships = Membership.objects.filter(user=user)
-            if memberships.exists():
-                projects = [membership.project for membership in memberships]
-                serialized_projects = ProjectSerializer(projects, many=True).data
-                return Response({ 'status': status.HTTP_200_OK, 'msg': "Success", 'data': serialized_projects }, status=status.HTTP_200_OK)
-        except Membership.DoesNotExist:
+        memberships = Membership.objects.filter(user=user)
+        if memberships.exists():
+            projects = [membership.project for membership in memberships]
+            serialized_projects = ProjectSerializer(projects, many=True).data
+            return Response({ 'status': status.HTTP_200_OK, 'msg': "Success", 'data': serialized_projects }, status=status.HTTP_200_OK)
+        else:
             return Response({ 'status': status.HTTP_404_NOT_FOUND, 'msg': "Project not found" }, status=status.HTTP_404_NOT_FOUND)
