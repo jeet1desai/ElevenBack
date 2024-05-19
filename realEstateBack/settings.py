2228e7e0dc1a3efc014e31ff71d3bfdcca58cddd
jeet1desai <jeetdesai995@gmail.com>
2024-05-19 19:08:09 +0530

    revert change

--- realEstateBack/settings.py
+++ realEstateBack/settings.py
@@ -32,7 +32,7 @@ CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:8000", "https
 
 # Application definition
 INSTALLED_APPS = [
-    'daphne',
+    "daphne",
 
     'django.contrib.admin',
     'django.contrib.auth',
